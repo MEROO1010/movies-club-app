2c7e7e140fbc46c1045dbe52ee4a66f37bfb4221
unknown <meroo1995saggaf@outlook.com>
2023-10-31 13:06:01 +0300

    start with the UUUI pI pages

---
+++ lib/pages/GetStarted.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class GetStarted extends StatefulWidget {
+  const GetStarted({super.key});
+
+  @override
+  State<GetStarted> createState() => _Splsh_screenState();
+}
+
+class _Splsh_screenState extends State<GetStarted> {
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: [
+        Container(
+          color: HexColor('#1A1E26'),
+        ),
+        //add logo
+        Container(
+          child: Image.asset(
+            'assets/images/logo.png',
+            width: 300,
+          ),
+          padding: EdgeInsets.only(left: 48, top: 80),
+        ),
+
+        //add button
+        Padding(
+          padding: EdgeInsets.only(left: 150, top: 100),
+          child: Container(
+            child: ElevatedButton(onPressed: () {}, child: Text('Get Started')),
+          ),
+        )
+      ],
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:movies_club/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const splash_screen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
