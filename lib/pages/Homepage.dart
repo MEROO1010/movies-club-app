a5d1683628a9a4bcb0dea18529c07cd7fffe40c5
unknown <meroo1995saggaf@outlook.com>
2023-11-03 02:42:57 +0300

    add search bar

--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -5,37 +5,44 @@ import 'package:movies_club/model/movies.dart';
 class MoviesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Movie List'),
-      ),
-      body: FutureBuilder<List<dynamic>>(
-        future: fetchMovies(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final movies = snapshot.data!;
-            return ListView.builder(
-              itemCount: movies.length,
-              itemBuilder: (context, index) {
-                final movie = Movie.fromJson(movies[index]);
-                return ListTile(
-                  title: Text(movie.title),
-                  subtitle: Text(movie.overview),
-                  leading: Image.network(movie.posterUrl),
-                );
-              },
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('Error: ${snapshot.error}'),
-            );
-          } else {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
+    return Wrap(
+      children: [
+        Container(
+          color: Color.fromRGBO(26, 30, 38, 1),
+        ),
+        Container(
+          child: Text(
+            'What do you want to watch?',
+            style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 20,
+                color: Colors.white,
+                decoration: TextDecoration.none),
+          ),
+          padding: EdgeInsets.only(left: 30, top: 40),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 28, left: 30),
+          child: Container(
+            width: 320,
+            height: 50,
+            child: Material(
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  hintText: 'Search..',
+                  filled: true,
+                  fillColor: Color.fromRGBO(255, 255, 255, 1),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(style: BorderStyle.none),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Container(),
+      ],
     );
   }
 }
