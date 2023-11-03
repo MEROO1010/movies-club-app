ea09c90d2a10915a16d971291d9a0b924e6297d3
unknown <meroo1995saggaf@outlook.com>
2023-11-03 10:25:58 +0300

    working on the homepage

--- lib/model/fetch_movies.dart
+++ lib/model/fetch_movies.dart
@@ -2,8 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future<List<dynamic>> fetchMovies() async {
-  final response = await http.get(Uri.parse(
-      'https://api.themoviedb.org/3/movie/11?api_key=bb0fb311aa848decc2057b5728d53445'));
+  final response = await http.get(Uri.parse('api key'));
 
   if (response.statusCode == 200) {
     final decodedData = jsonDecode(response.body);
--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:movies_club/model/fetch_movies.dart';
 import 'package:movies_club/model/movies.dart';
 
@@ -41,7 +43,90 @@ class MoviesScreen extends StatelessWidget {
             ),
           ),
         ),
-        Container(),
+        Stack(
+          children: [
+            Container(
+              child: SingleChildScrollView(
+                  physics: BouncingScrollPhysics(),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Trending Movies',
+                          style: GoogleFonts.roboto(
+                              fontSize: 20,
+                              textStyle: TextStyle(
+                                  decoration: TextDecoration.none,
+                                  color: Colors.white)),
+                        ),
+                        const SizedBox(
+                          height: 32,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: CarouselSlider.builder(
+                              itemCount: 10,
+                              options: CarouselOptions(
+                                  height: 300,
+                                  autoPlay: true,
+                                  viewportFraction: 0.55,
+                                  enlargeCenterPage: true,
+                                  pageSnapping: true,
+                                  autoPlayCurve: Curves.fastOutSlowIn,
+                                  autoPlayAnimationDuration:
+                                      const Duration(seconds: 2)),
+                              itemBuilder: (context, itemIndex, pageViewIndex) {
+                                return ClipRRect(
+                                  borderRadius: BorderRadius.circular(12),
+                                  child: Container(
+                                    height: 300,
+                                    width: 200,
+                                    color: Colors.amber,
+                                  ),
+                                );
+                              }),
+                        ),
+                        const SizedBox(height: 32),
+                        Text(
+                          'Top rated movies',
+                          style: GoogleFonts.roboto(
+                              fontSize: 20,
+                              textStyle: TextStyle(
+                                decoration: TextDecoration.none,
+                                color: Colors.white,
+                              )),
+                        ),
+                        const SizedBox(height: 32),
+                        SizedBox(
+                          height: 200,
+                          width: double.infinity,
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            physics: const BouncingScrollPhysics(),
+                            itemCount: 10,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(8),
+                                  child: Container(
+                                    color: Colors.amber,
+                                    height: 200,
+                                    width: 150,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  )),
+            ),
+          ],
+        )
       ],
     );
   }
